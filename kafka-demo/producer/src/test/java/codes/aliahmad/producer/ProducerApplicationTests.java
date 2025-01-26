32c801b3ac3308f2806532c7424ee40214c57580
Ali Ahmad <aliahmadcse@gmail.com>
2025-01-26 22:03:37 +0500

    restructure

---
+++ kafka-demo/producer/src/test/java/codes/aliahmad/producer/ProducerApplicationTests.java
@@ -0,0 +1,15 @@
+package codes.aliahmad.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
